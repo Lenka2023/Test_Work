8f038e3a17e4f14881017de09a979088f365ff45
Lenka2023 <lenka20231@gmail.com>
2019-05-02 19:27:52 +0300

    include all js files

--- resources/views/spa.blade.php
+++ resources/views/spa.blade.php
@@ -4,18 +4,17 @@
     <meta charset="UTF-8">
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <link rel="stylesheet" type="text/css" href="/css/style.min.css">
+    <link rel="stylesheet" type="text/css" href="{{ asset('css/style.min.css') }}">
     <title>Document</title>
 </head>
 
 <body>
     <div id="app"></div>
 
-    <script src="{{ asset('js/app.js') }}"></script>
-
-    <script src="/plugins/jquery-1.8.1.js"></script>
-    <script src="/plugins/slick/slick.min.js"></script>
-    <script src="/js/common.js"></script>
+     <script src="{{ asset('/plugins/slick/slick.min.js') }}"></script>
+      <script src="{{ asset('js/app.js') }}"></script>
+      <script src="{{ asset('/plugins/jquery-1.8.1.js') }}"></script>
+      <script src="{{ asset('js/common.js') }}"></script>
 
     <script>
         WebFontConfig = {
